ce16dbf11a39d537be19c80f152a8d21eca31bc4
Diego Araujo <ferreiradearaujod@gmail.com>
2024-07-08 11:24:34 -0300

    feat: adicionando manipulação de string

---
+++ script.js
@@ -0,0 +1,13 @@
+var nome = "Diego"
+console.log("Seu mome é", nome)
+
+// template string
+console.log(`Seu nome é ${nome}`);
+
+// exercício
+var nome = "Diego"
+var idade = 30
+var time = "Ceará"
+var cidade = "Fortaleza"
+
+console.log(`${nome} tem ${idade} torce para ${time} mora em ${cidade}`);
